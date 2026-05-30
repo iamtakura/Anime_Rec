2ef2b68868df6a2767e2ed15aeaaaae79891d096
Takura <300takura@gmail.com>
2026-05-30 17:02:51 +0200

    chore: setup deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://iamtakura.github.io',
+  base: '/Anime_Rec',
   output: 'static'
 });
